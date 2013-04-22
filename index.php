6bab8e64cd72a234de65d9bf7fdb80b5849b39f2
Ryan Maskell <MaskellRyan@me.com>
2013-04-22 00:23:20 -0700

    Nearing completion

--- index.php
+++ index.php
@@ -29,15 +29,24 @@
 </head>
 
 <body>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="landing">
   <div id="parallax-frame">
     <img id="clouds" class="paraimg" src="assets/img/clouds.png" />
     <img id="seattle" class="paraimg" src="assets/img/seattle.png" />
     <img id="ryan" class="paraimg" src="assets/img/Ryan.png" />
     <div id="greeting">
-      <h1>Salutations...</h1>
-      <p>I'm Ryan, and this site is under construction.
-           <br> <em>Come back later! Or email me: <a href="mailto:maskellryan@me.com">MaskellRyan@me.com</a></em>
+      <h1>Hello! I'm <span>Ryan Maskell</span></h1>
+      <p>Welcome to my online presence, here you can learn anything about me and connect.
+      </p>
+      <p><em>If you just want to get ahold of me, send me an email: <a href="mailto:maskellryan@me.com">MaskellRyan@me.com</a></em>
       </p>
     </div>
   </div>
@@ -46,38 +55,42 @@
 <div class="wrap">
   <div class="row-fluid">
     <div class="span4">
-      <img class="img-polaroid" src="http://placehold.it/330x200" />
+      <img class="img-polaroid" src="assets/img/vendetta.jpg" />
       <h2>Enthusiast</h2>
-      <p>Explain how I love everything and how I become passionate about things that I think are cool and then try and tell everyone about them.</p>
+      <p>I have an intesnse passion for learning new things, though my topics of interest can sometimes be narrow. I have a wide apreciation for technology and the arts. I am always playing with a new online service or app, and am constantly looking at inpiring works of art. I love words, poetry, and literature. I become engrossed in books, subjects, and science.</p>
     </div>
     <div class="span4">
-      <img class="img-polaroid" src="http://placehold.it/330x200" />
-      <h2>Optimist</h2>
-      <p>Words on how I view the world, my philosophies, and why I get out of bed in the morning. Basically what I care about most.</p>
+      <img class="img-polaroid" src="assets/img/spirited.jpg" />
+      <h2>Spirited</h2>
+      <p>I am my own person. That is unquestionable. I stay true to who I want to be and what I value. I am fun-loving, optimistic, and energetic. I like to have a good time even whilst doing the most mundane things, because I beleive life's greatest treasures lie in the little things. I dance badly, sing loudly, and can be a obnoxious goofball. I love life, and I get out of bed most mornings with both feet springing with energy and ambition.</p>
     </div>
     <div class="span4">
-      <img class="img-polaroid" src="http://placehold.it/330x200" />
+      <img class="img-polaroid" src="assets/img/summon.jpg" />
       <h2>Designer</h2>
-      <p>Things about my education, training, and how I became a graphic & web designer. How it is my passion and my true calling.</p>
+      <p>When I was young I never thought I was creative, but now after years of being trained, tested, and pushed to my limits. I have come out the other side a Designer. My motiviation to learn and strive to be the best in my feild has caught some recognition too; I have won awards in various design diciplines on the local, state, and national levels of competiton. My personal design brand is called MaskellMedia.</p>
     </div>
   </div>
   <div class="row-fluid">
     <div class="media">
-      <a class="pull-right" href="#">
-        <img class="media-object img-polaroid" src="http://placehold.it/420x220">
+      <a style="border-bottom-width: 0;" class="pull-right" target="_blank" href="http://www.MaskellMedia.com">
+        <img class="media-object img-polaroid" src="assets/img/maskellmedia.jpg">
       </a>
       <div class="media-body">
         <h2 class="media-heading">MaskellMedia</h2>
-        <p>Stuff about my Career, goals, links to MaskellMedia, and a brief overview of my accomplishments and current status.</p>
+        <p>My design company is a freelance multimedia production service. I offer my skills as a Photographer, Graphic Designer, Web Designer, & Video Producer and all the little jobs that come with those. If you are interested in working with me, send me an email at <a href="mailto:Ryan@MaskellMedia.com">Ryan@MaskellMedia.com</a></p>
+        <p>Otherwise, please check out <a target="_blank" href="http://www.MaskellMedia.com">the website</a>, like MaskellMedia on <a target="_blank" href="http://www.facebook.com/MaskellMedia">facebook</a>, or follow on <a target="_blank" href="http://www.twitter.com/MaskellMedia">twitter</a> or <a target="_blank" href="http://www.dribbble.com/MaskellMedia">dribbble</a> to see my work and stay up to date on my projects.</p>
+<p>        <div class="fb-like" data-href="http://www.facebook.com/MaskellMedia" data-send="true" data-width="450" data-show-faces="true" data-font="tahoma" data-colorscheme="dark"></div></p>
+<p>        <a href="https://twitter.com/MaskellMedia" class="twitter-follow-button" data-show-count="false">Follow @MaskellMedia</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></p>
       </div>
     </div>
   </div>
 </div>
 
-<footer>
+<div id="social">
   <img class="bg-img" src="assets/img/chicago-predark.jpg">
   <img class="bg-img bg-hover" src="assets/img/chicago-dark3.jpg">
-  <h3 class="full-header">Lets Get Social!</h3>
+  <h3 class="full-header">Shall we Socialize?</h3>
 
   <div class="wrap">
 
@@ -86,14 +99,14 @@
       <div class="span4 clearfix">
 
       <h3>Facebook</h3>
-      <a href="http://www.facebook.com/maskellryan"><img class="avatar img-polaroid" src="https://graph.facebook.com/maskellryan/picture?type=large"></a>
+      <a target="_blank" href="http://www.facebook.com/maskellryan"><img class="avatar img-polaroid" src="https://graph.facebook.com/maskellryan/picture?type=large"></a>
       <p>Have you met me? In person? Then chances are I'll add you back.</p>
       <form action="http://www.facebook.com/addfriend.php?id=maskellryan"><input type="hidden" name="id" value="100000296041161" /><input type="submit" value="Friend Me" /></form>
     </div>
     <div class="span8 clearfix">
 
       <h3>Tumblr</h3>
-      <a href="http://www.passionatemeandering.tumblr.com"><img class="avatar img-polaroid" src="http://api.tumblr.com/v2/blog/passionatemeandering.tumblr.com/avatar/512"></a>
+      <a target="_blank" href="http://www.passionatemeandering.tumblr.com"><img class="avatar img-polaroid" src="http://api.tumblr.com/v2/blog/passionatemeandering.tumblr.com/avatar/512"></a>
       <p>The content you will see in my blog will be an amalgamation of beautiful art, ridiculous memes, heartfelt gifs, Anti-Religious posts, and pictures of Hayley Williams from the band Paramore. </p> 
       <iframe class="btn" frameborder="0" border="0" scrolling="no" allowtransparency="true" height="25" width="115" src="http://platform.tumblr.com/v1/follow_button.html?button_type=2&tumblelog=passionatemeandering&color_scheme=light"></iframe>
     </div>
@@ -102,7 +115,7 @@
     <div class="span4">
 
       <h3>Twitter</h3>
-      <a href="http://www.twitter.com/maskellryan"><img class="avatar img-polaroid" src="https://api.twitter.com/1/users/profile_image/maskellryan?size=original"></a>
+      <a target="_blank" href="http://www.twitter.com/maskellryan"><img class="avatar img-polaroid" src="https://api.twitter.com/1/users/profile_image/maskellryan?size=original"></a>
 
       <p>I tweet a lot of random stuff, personal life things, & some design related tweets too.</p>
 
@@ -117,7 +130,7 @@
       <div class="span8">
 
         <h3>Instagram</h3>
-        <p>I love photography, and Instagram is the perfect outlet that is always at my fingertips, check out my photos, or go straight to <a href="http://www.instagram.com/maskellryan">my profile here</a>.</p>
+        <p>I love photography, and Instagram is the perfect outlet that is always at my fingertips, check out my photos, or go straight to <a target="_blank" href="http://www.instagram.com/maskellryan">my profile here</a>.</p>
         <?php
           // Supply a user id and an access token
           $userid = "30961079";
@@ -148,11 +161,12 @@
       </div>
 
     </div>
-    <div class="row-fluid">
+    <div style="margin-top: -40px;" class="row-fluid">
       <div class="span12"> 
-        <h3>Music I'm Listening to</h3>
-        <p>Follow me on Last.FM to get a taste of my eclectic stylings.</p>
-        <div id="lastfm">
+        <h3>The Music in my head</h3>
+        <p>I listen to everything from String Quartets and poppy sing-alongs to Heavy Rap-Rock, but mainly alternative punk is my go to genre. My favorite band of all time is PANIC! at the Disco, although others that inspire and give me chills are My Chemical Romance, Fall Out Boy, Paramore, Sum 41, Hollywood Undead, and Daft Punk. Follow me on <a target="_blank" href="http://www.last.fm/user/maskellryan">Last.FM</a> to learn more about my eclectic taste in music.</p>
+        <p style="text-align:center;"> <em>Now playing:</em> </p>
+        <div id="lastfm" class="clearfix">
           <dl class="lastplay">
             <dt class="lfm_art"></dt>
             <dd class="lfm_song"> </dd>
@@ -163,11 +177,101 @@
       </div>
     </div>
   </div>
-</footer>
+</div> <!-- END SOCIAL -->
+
+<footer>
+  <h1 class="full-header">Let's talk</h1>
+<div class="wrap">
+
+<h4>Send me an email and we can chat about anything you'd like!</h4>
+
+  <div id="contact" class="section">
+    <div >
+    
+          <?php if(isset($emailSent) && $emailSent == true) { ?>
+                <p class="info">Your email was sent. Huzzah!</p>
+            <?php } else { ?>
+
+        <div id="contact-form">
+          <?php if(isset($hasError) || isset($captchaError) ) { ?>
+                        <p class="alert">Error submitting the form</p>
+                    <?php } ?>
+        
+          <form id="contact-us" action="contact.php" method="post">
+            <div class="formblock">
+              <label class="screen-reader-text">Name</label>
+              <input type="text" name="contactName" id="contactName" value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>" class="txt requiredField" placeholder="Name:" />
+              <?php if($nameError != '') { ?>
+                <br /><span class="error"><?php echo $nameError;?></span> 
+              <?php } ?>
+            </div>
+                        
+            <div class="formblock">
+              <label class="screen-reader-text">Email</label>
+              <input type="text" name="email" id="email" value="<?php if(isset($_POST['email']))  echo $_POST['email'];?>" class="txt requiredField email" placeholder="Email:" />
+              <?php if($emailError != '') { ?>
+                <br /><span class="error"><?php echo $emailError;?></span>
+              <?php } ?>
+            </div>
+                        
+            <div class="formblock">
+              <label class="screen-reader-text">Message</label>
+               <textarea name="comments" id="commentsText" class="txtarea requiredField" placeholder="Message:"><?php if(isset($_POST['comments'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['comments']); } else { echo $_POST['comments']; } } ?></textarea>
+              <?php if($commentError != '') { ?>
+                <br /><span class="error"><?php echo $commentError;?></span> 
+              <?php } ?>
+            </div>
+                        
+              <button name="submit" type="submit" class="subbutton">Send Email</button>
+              <input type="hidden" name="submitted" id="submitted" value="true" />
+          </form>     
+        </div>
+        
+      <?php } ?>
+    </div>
+    </div><!-- End #contact -->
 
 </div>
 
+</footer>
+
   <!-- JavaScript at the bottom for fast page loading -->
+<script type="text/javascript">
+  <!--//--><![CDATA[//><!--
+  $(document).ready(function() {
+    $('form#contact-us').submit(function() {
+      $('form#contact-us .error').remove();
+      var hasError = false;
+      $('.requiredField').each(function() {
+        if($.trim($(this).val()) == '') {
+          var labelText = $(this).prev('label').text();
+          $(this).parent().append('<span class="error">Your forgot to enter your '+labelText+'.</span>');
+          $(this).addClass('inputError');
+          hasError = true;
+        } else if($(this).hasClass('email')) {
+          var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+          if(!emailReg.test($.trim($(this).val()))) {
+            var labelText = $(this).prev('label').text();
+            $(this).parent().append('<span class="error">Sorry! You\'ve entered an invalid '+labelText+'.</span>');
+            $(this).addClass('inputError');
+            hasError = true;
+          }
+        }
+      });
+      if(!hasError) {
+        var formInput = $(this).serialize();
+        $.post($(this).attr('action'),formInput, function(data){
+          $('form#contact-us').slideUp("fast", function() {          
+            $(this).before('<div class="thumbsup"><img class="img-polaroid" src="assets/img/thumbsup.jpg"></div><p class="tick center"><strong>You did it!</strong> Thanks for the message! I will reply as soon as I can, meanwhile, why dont you check out <a target="_blank" href="http://www.MaskellMedia.com">MaskellMedia.com</a>?.</p>');
+          });
+        });
+      }
+      
+      return false; 
+    });
+  });
+  //-->!]]>
+</script>
 
   
   <script src="assets/js/bootstrap.js"></script>
