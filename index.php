83b1a342c16c5cda0d906a8aed650a2f42388568
Ryan Maskell <MaskellRyan@me.com>
2013-04-26 09:52:49 -0700

    null

--- index.php
+++ index.php
@@ -26,6 +26,20 @@
   <script src="assets/js/jquery.fancybox.js"></script>
   <script type="text/javascript" src="assets/js/plax.js"></script>
 
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30400864-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
+
 </head>
 
 <body>
